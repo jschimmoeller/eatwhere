285004a33d5db19f5bdc2fd449002503fdbcccac
jschimmoeller <jschimmoeller@star2star.com>
2017-09-20 13:34:25 -0400

    initial file config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import {Text, View} from 'react-native';
 import Config from './configs/firebase';
+import * as firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp(Config.firebaseConfig);
+
+console.log('fa:', firebaseApp)
 
 class App extends Component {
   render() {
